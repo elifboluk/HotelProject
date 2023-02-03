58c08dba05d8f51e0ba329e563d8129733b0043e
elifboluk <elifbolukk@gmail.com>
2023-02-03 23:36:47 +0300

    Misafir Formu Revizeleri

--- OtelProject/Formlar/Misafir/FrmMisafirKarti.cs
+++ OtelProject/Formlar/Misafir/FrmMisafirKarti.cs
@@ -26,43 +26,47 @@ namespace OtelProject.Formlar.Misafir
         string resim1, resim2;
         private void FrmMisafirKarti_Load(object sender, EventArgs e)
         {
-            // Güncellenecek kart bilgileri
-            if (id != 0) //Form yüklendiğinde id sıfır değilse aşağıdaki işlemleri yap.
+            try
             {
-                var misafir = repo.Find(x => x.MisafirID == id);
-                TxtAdSoyad.Text = misafir.AdSoyad;
-                TxtTc.Text = misafir.TC;
-                TxtAdres.Text = misafir.Adres;
-                TxtTelefon.Text = misafir.Telefon;
-                TxtMail.Text = misafir.Mail;                
-                TxtAciklama.Text = misafir.Aciklama;                
-                // PictureEditKimlikOn.Image = Image.FromFile(misafir.KimlikFoto1); // Kimliğin ön kısmındaki fotoğrafı aldık.
-                // PictureEditKimlikArka.Image = Image.FromFile(misafir.KimlikFoto2); // Kimliğin arka kısmındaki fotoğrafı aldık.
-                resim1 = misafir.KimlikFoto1;
-                resim2 = misafir.KimlikFoto2;
-                lookUpEditSehir.EditValue = misafir.sehir; // Misafir kartına şehir bilgisi geldi.
-                lookUpEditUlke.EditValue = misafir.Ulke; // Misafir kartına ülke bilgisi geldi.
-                lookUpEditilce.EditValue = misafir.ilce; // Misafir kartına ilçe bilgisi geldi.
+                // Güncellenecek kart bilgileri
+                if (id != 0) //Form yüklendiğinde id sıfır değilse aşağıdaki işlemleri yap.
+                {
+                    var misafir = repo.Find(x => x.MisafirID == id);
+                    TxtAdSoyad.Text = misafir.AdSoyad;
+                    TxtTc.Text = misafir.TC;
+                    TxtAdres.Text = misafir.Adres;
+                    TxtTelefon.Text = misafir.Telefon;
+                    TxtMail.Text = misafir.Mail;
+                    TxtAciklama.Text = misafir.Aciklama;
+                    // PictureEditKimlikOn.Image = Image.FromFile(misafir.KimlikFoto1); // Kimliğin ön kısmındaki fotoğrafı aldık.
+                    // PictureEditKimlikArka.Image = Image.FromFile(misafir.KimlikFoto2); // Kimliğin arka kısmındaki fotoğrafı aldık.
+                    resim1 = misafir.KimlikFoto1;
+                    resim2 = misafir.KimlikFoto2;
+                    lookUpEditSehir.EditValue = misafir.sehir; // Misafir kartına şehir bilgisi geldi.
+                    lookUpEditUlke.EditValue = misafir.Ulke; // Misafir kartına ülke bilgisi geldi.
+                    lookUpEditilce.EditValue = misafir.ilce; // Misafir kartına ilçe bilgisi geldi.
+                }
 
+                // Ülke Listesi
+                lookUpEditUlke.Properties.DataSource = (from x in db.TblUlke
+                                                        select new
+                                                        {
+                                                            x.UlkeID,
+                                                            x.UlkeAd
+                                                        }).ToList();
 
-
+                // Şehir Listesi
+                lookUpEditSehir.Properties.DataSource = (from x in db.iller
+                                                         select new
+                                                         {
+                                                             Id = x.id,
+                                                             Şehir = x.sehir
+                                                         }).ToList();
             }
-
-            // Ülke Listesi
-            lookUpEditUlke.Properties.DataSource = (from x in db.TblUlke
-                                                    select new
-                                                    {
-                                                        x.UlkeID,
-                                                        x.UlkeAd
-                                                    }).ToList();
-
-            // Şehir Listesi
-            lookUpEditSehir.Properties.DataSource = (from x in db.iller
-                                                    select new
-                                                    {
-                                                        Id=x.id,
-                                                        Şehir=x.sehir
-                                                    }).ToList();
+            catch (Exception)
+            {
+                XtraMessageBox.Show("Bir hata oluştu lütfen sütunları kontrol edin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
         }
 
         private void lookUpEditUlke_EditValueChanged(object sender, EventArgs e)
@@ -74,15 +78,15 @@ namespace OtelProject.Formlar.Misafir
         {
             int secilen;
             secilen = int.Parse(lookUpEditSehir.EditValue.ToString());
-            lookUpEditilce.Properties.DataSource=(from x in db.ilceler
-                                                  select new
-                                                  {
-                                                     Id= x.id,
-                                                     İlçe=x.ilce, 
-                                                     Şehir = x.sehir
-                                                  } ).Where(y=>y.Şehir==secilen).ToList();
+            lookUpEditilce.Properties.DataSource = (from x in db.ilceler
+                                                    select new
+                                                    {
+                                                        Id = x.id,
+                                                        İlçe = x.ilce,
+                                                        Şehir = x.sehir
+                                                    }).Where(y => y.Şehir == secilen).ToList();
         }
-        
+
 
         private void PictureEditKimlikOn_EditValueChanged(object sender, EventArgs e)
         {
@@ -129,7 +133,7 @@ namespace OtelProject.Formlar.Misafir
             t.Durum = 1;
             t.sehir = int.Parse(lookUpEditSehir.EditValue.ToString());
             t.ilce = int.Parse(lookUpEditilce.EditValue.ToString());
-            t.Ulke = int.Parse(lookUpEditUlke.EditValue.ToString());            
+            t.Ulke = int.Parse(lookUpEditUlke.EditValue.ToString());
             t.KimlikFoto1 = resim1;
             t.KimlikFoto2 = resim2;
             repo.TAdd(t);
