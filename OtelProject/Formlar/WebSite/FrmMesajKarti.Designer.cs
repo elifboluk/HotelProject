22bfafed07ba44d00694d497e989ac1f38d4ecdb
elifboluk <elifbolukk@gmail.com>
2023-02-15 23:16:58 +0300

    Gelen Mesaj Detayları

--- OtelProject/Formlar/WebSite/FrmMesajKarti.Designer.cs
+++ OtelProject/Formlar/WebSite/FrmMesajKarti.Designer.cs
@@ -30,40 +30,41 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMesajKarti));
             this.groupControl6 = new DevExpress.XtraEditors.GroupControl();
-            this.TxtAciklama = new DevExpress.XtraEditors.MemoEdit();
+            this.TxtMesaj = new DevExpress.XtraEditors.MemoEdit();
             this.labelControl13 = new DevExpress.XtraEditors.LabelControl();
             this.pictureEdit7 = new DevExpress.XtraEditors.PictureEdit();
             this.TxtAdSoyad = new DevExpress.XtraEditors.GroupControl();
-            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.TxtGonderenMail = new DevExpress.XtraEditors.TextEdit();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.pictureEdit3 = new DevExpress.XtraEditors.PictureEdit();
             this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
             this.groupControl3 = new DevExpress.XtraEditors.GroupControl();
-            this.TxtMail = new DevExpress.XtraEditors.TextEdit();
-            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
-            this.pictureEdit3 = new DevExpress.XtraEditors.PictureEdit();
-            this.labelControl11 = new DevExpress.XtraEditors.LabelControl();
-            this.TxtTelefon = new DevExpress.XtraEditors.TextEdit();
-            this.BtnVazgec = new DevExpress.XtraEditors.SimpleButton();
             this.pictureEdit4 = new DevExpress.XtraEditors.PictureEdit();
+            this.TxtKonu = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl11 = new DevExpress.XtraEditors.LabelControl();
+            this.BtnVazgec = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnGonder = new DevExpress.XtraEditors.SimpleButton();
+            this.TxtGonAdSoyad = new DevExpress.XtraEditors.TextEdit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl6)).BeginInit();
             this.groupControl6.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.TxtAciklama.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtMesaj.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit7.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.TxtAdSoyad)).BeginInit();
             this.TxtAdSoyad.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtGonderenMail.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit3.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl3)).BeginInit();
             this.groupControl3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.TxtMail.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit3.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.TxtTelefon.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit4.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtKonu.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtGonAdSoyad.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // groupControl6
             // 
-            this.groupControl6.Controls.Add(this.TxtAciklama);
+            this.groupControl6.Controls.Add(this.TxtMesaj);
             this.groupControl6.Controls.Add(this.labelControl13);
             this.groupControl6.Controls.Add(this.pictureEdit7);
             this.groupControl6.Location = new System.Drawing.Point(12, 124);
@@ -73,12 +74,12 @@
             this.groupControl6.TabIndex = 44;
             this.groupControl6.Text = "groupControl6";
             // 
-            // TxtAciklama
+            // TxtMesaj
             // 
-            this.TxtAciklama.Location = new System.Drawing.Point(116, 5);
-            this.TxtAciklama.Name = "TxtAciklama";
-            this.TxtAciklama.Size = new System.Drawing.Size(331, 179);
-            this.TxtAciklama.TabIndex = 4;
+            this.TxtMesaj.Location = new System.Drawing.Point(116, 5);
+            this.TxtMesaj.Name = "TxtMesaj";
+            this.TxtMesaj.Size = new System.Drawing.Size(331, 179);
+            this.TxtMesaj.TabIndex = 4;
             // 
             // labelControl13
             // 
@@ -103,7 +104,7 @@
             // 
             // TxtAdSoyad
             // 
-            this.TxtAdSoyad.Controls.Add(this.textEdit1);
+            this.TxtAdSoyad.Controls.Add(this.TxtGonderenMail);
             this.TxtAdSoyad.Controls.Add(this.labelControl1);
             this.TxtAdSoyad.Controls.Add(this.pictureEdit3);
             this.TxtAdSoyad.Location = new System.Drawing.Point(12, 31);
@@ -113,13 +114,13 @@
             this.TxtAdSoyad.TabIndex = 42;
             this.TxtAdSoyad.Text = "groupControl1";
             // 
-            // textEdit1
+            // TxtGonderenMail
             // 
-            this.textEdit1.EditValue = "";
-            this.textEdit1.Location = new System.Drawing.Point(116, 9);
-            this.textEdit1.Name = "textEdit1";
-            this.textEdit1.Size = new System.Drawing.Size(331, 20);
-            this.textEdit1.TabIndex = 3;
+            this.TxtGonderenMail.EditValue = "";
+            this.TxtGonderenMail.Location = new System.Drawing.Point(116, 9);
+            this.TxtGonderenMail.Name = "TxtGonderenMail";
+            this.TxtGonderenMail.Size = new System.Drawing.Size(331, 20);
+            this.TxtGonderenMail.TabIndex = 3;
             // 
             // labelControl1
             // 
@@ -128,6 +129,19 @@
             this.labelControl1.Size = new System.Drawing.Size(68, 13);
             this.labelControl1.TabIndex = 1;
             this.labelControl1.Text = "Gönderen Mail";
+            // 
+            // pictureEdit3
+            // 
+            this.pictureEdit3.EditValue = ((object)(resources.GetObject("pictureEdit3.EditValue")));
+            this.pictureEdit3.Location = new System.Drawing.Point(15, 9);
+            this.pictureEdit3.Name = "pictureEdit3";
+            this.pictureEdit3.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pictureEdit3.Properties.Appearance.Options.UseBackColor = true;
+            this.pictureEdit3.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pictureEdit3.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit3.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pictureEdit3.Size = new System.Drawing.Size(20, 20);
+            this.pictureEdit3.TabIndex = 27;
             // 
             // pictureEdit1
             // 
@@ -144,72 +158,18 @@
             // 
             // groupControl3
             // 
+            this.groupControl3.Controls.Add(this.TxtGonAdSoyad);
             this.groupControl3.Controls.Add(this.pictureEdit4);
-            this.groupControl3.Controls.Add(this.TxtMail);
+            this.groupControl3.Controls.Add(this.TxtKonu);
             this.groupControl3.Controls.Add(this.pictureEdit1);
             this.groupControl3.Controls.Add(this.labelControl3);
             this.groupControl3.Controls.Add(this.labelControl11);
-            this.groupControl3.Controls.Add(this.TxtTelefon);
             this.groupControl3.Location = new System.Drawing.Point(12, 79);
             this.groupControl3.Name = "groupControl3";
             this.groupControl3.ShowCaption = false;
             this.groupControl3.Size = new System.Drawing.Size(470, 39);
             this.groupControl3.TabIndex = 43;
             this.groupControl3.Text = "groupControl3";
-            // 
-            // TxtMail
-            // 
-            this.TxtMail.EditValue = "";
-            this.TxtMail.Location = new System.Drawing.Point(336, 9);
-            this.TxtMail.Name = "TxtMail";
-            this.TxtMail.Size = new System.Drawing.Size(110, 20);
-            this.TxtMail.TabIndex = 29;
-            // 
-            // labelControl3
-            // 
-            this.labelControl3.Location = new System.Drawing.Point(269, 12);
-            this.labelControl3.Name = "labelControl3";
-            this.labelControl3.Size = new System.Drawing.Size(24, 13);
-            this.labelControl3.TabIndex = 28;
-            this.labelControl3.Text = "Konu";
-            // 
-            // pictureEdit3
-            // 
-            this.pictureEdit3.EditValue = ((object)(resources.GetObject("pictureEdit3.EditValue")));
-            this.pictureEdit3.Location = new System.Drawing.Point(15, 9);
-            this.pictureEdit3.Name = "pictureEdit3";
-            this.pictureEdit3.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
-            this.pictureEdit3.Properties.Appearance.Options.UseBackColor = true;
-            this.pictureEdit3.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.pictureEdit3.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
-            this.pictureEdit3.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
-            this.pictureEdit3.Size = new System.Drawing.Size(20, 20);
-            this.pictureEdit3.TabIndex = 27;
-            // 
-            // labelControl11
-            // 
-            this.labelControl11.Location = new System.Drawing.Point(41, 12);
-            this.labelControl11.Name = "labelControl11";
-            this.labelControl11.Size = new System.Drawing.Size(46, 13);
-            this.labelControl11.TabIndex = 4;
-            this.labelControl11.Text = "Ad Soyad";
-            // 
-            // TxtTelefon
-            // 
-            this.TxtTelefon.EditValue = "";
-            this.TxtTelefon.Location = new System.Drawing.Point(116, 9);
-            this.TxtTelefon.Name = "TxtTelefon";
-            this.TxtTelefon.Size = new System.Drawing.Size(110, 20);
-            this.TxtTelefon.TabIndex = 2;
-            // 
-            // BtnVazgec
-            // 
-            this.BtnVazgec.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnVazgec.ImageOptions.Image")));
-            this.BtnVazgec.Location = new System.Drawing.Point(379, 319);
-            this.BtnVazgec.Name = "BtnVazgec";
-            this.BtnVazgec.Size = new System.Drawing.Size(103, 30);
-            this.BtnVazgec.TabIndex = 45;
-            this.BtnVazgec.Text = "Vazgeç";
             // 
             // pictureEdit4
             // 
@@ -224,11 +184,62 @@
             this.pictureEdit4.Size = new System.Drawing.Size(20, 20);
             this.pictureEdit4.TabIndex = 46;
             // 
+            // TxtKonu
+            // 
+            this.TxtKonu.EditValue = "";
+            this.TxtKonu.Location = new System.Drawing.Point(336, 9);
+            this.TxtKonu.Name = "TxtKonu";
+            this.TxtKonu.Size = new System.Drawing.Size(110, 20);
+            this.TxtKonu.TabIndex = 29;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(269, 12);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(24, 13);
+            this.labelControl3.TabIndex = 28;
+            this.labelControl3.Text = "Konu";
+            // 
+            // labelControl11
+            // 
+            this.labelControl11.Location = new System.Drawing.Point(41, 12);
+            this.labelControl11.Name = "labelControl11";
+            this.labelControl11.Size = new System.Drawing.Size(46, 13);
+            this.labelControl11.TabIndex = 4;
+            this.labelControl11.Text = "Ad Soyad";
+            // 
+            // BtnVazgec
+            // 
+            this.BtnVazgec.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnVazgec.ImageOptions.Image")));
+            this.BtnVazgec.Location = new System.Drawing.Point(379, 319);
+            this.BtnVazgec.Name = "BtnVazgec";
+            this.BtnVazgec.Size = new System.Drawing.Size(103, 30);
+            this.BtnVazgec.TabIndex = 45;
+            this.BtnVazgec.Text = "Vazgeç";
+            // 
+            // BtnGonder
+            // 
+            this.BtnGonder.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnKaydet.ImageOptions.Image")));
+            this.BtnGonder.Location = new System.Drawing.Point(244, 319);
+            this.BtnGonder.Name = "BtnGonder";
+            this.BtnGonder.Size = new System.Drawing.Size(103, 30);
+            this.BtnGonder.TabIndex = 46;
+            this.BtnGonder.Text = "Gönder";
+            // 
+            // TxtGonAdSoyad
+            // 
+            this.TxtGonAdSoyad.EditValue = "";
+            this.TxtGonAdSoyad.Location = new System.Drawing.Point(116, 9);
+            this.TxtGonAdSoyad.Name = "TxtGonAdSoyad";
+            this.TxtGonAdSoyad.Size = new System.Drawing.Size(110, 20);
+            this.TxtGonAdSoyad.TabIndex = 47;
+            // 
             // FrmMesajKarti
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(494, 361);
+            this.Controls.Add(this.BtnGonder);
             this.Controls.Add(this.BtnVazgec);
             this.Controls.Add(this.groupControl6);
             this.Controls.Add(this.TxtAdSoyad);
@@ -238,20 +249,20 @@
             ((System.ComponentModel.ISupportInitialize)(this.groupControl6)).EndInit();
             this.groupControl6.ResumeLayout(false);
             this.groupControl6.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.TxtAciklama.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtMesaj.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit7.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.TxtAdSoyad)).EndInit();
             this.TxtAdSoyad.ResumeLayout(false);
             this.TxtAdSoyad.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtGonderenMail.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit3.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl3)).EndInit();
             this.groupControl3.ResumeLayout(false);
             this.groupControl3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.TxtMail.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit3.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.TxtTelefon.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit4.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtKonu.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtGonAdSoyad.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -259,20 +270,21 @@
         #endregion
 
         private DevExpress.XtraEditors.GroupControl groupControl6;
-        private DevExpress.XtraEditors.MemoEdit TxtAciklama;
+        private DevExpress.XtraEditors.MemoEdit TxtMesaj;
         private DevExpress.XtraEditors.LabelControl labelControl13;
         private DevExpress.XtraEditors.PictureEdit pictureEdit7;
         private DevExpress.XtraEditors.GroupControl TxtAdSoyad;
-        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit TxtGonderenMail;
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.PictureEdit pictureEdit1;
         private DevExpress.XtraEditors.GroupControl groupControl3;
-        private DevExpress.XtraEditors.TextEdit TxtMail;
+        private DevExpress.XtraEditors.TextEdit TxtKonu;
         private DevExpress.XtraEditors.LabelControl labelControl3;
         private DevExpress.XtraEditors.PictureEdit pictureEdit3;
         private DevExpress.XtraEditors.LabelControl labelControl11;
-        private DevExpress.XtraEditors.TextEdit TxtTelefon;
         private DevExpress.XtraEditors.SimpleButton BtnVazgec;
         private DevExpress.XtraEditors.PictureEdit pictureEdit4;
+        private DevExpress.XtraEditors.SimpleButton BtnGonder;
+        private DevExpress.XtraEditors.TextEdit TxtGonAdSoyad;
     }
 }
