dca775468cf0f387dc0443786dad941698833d8d
elifboluk <elifbolukk@gmail.com>
2023-04-08 21:47:08 +0300

    Grafikler tamamlandı.

---
+++ OtelProject/Formlar/Grafikler/FrmGrafik2.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.XtraCharts;
+using OtelProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtelProject.Formlar.Grafikler
+{
+    public partial class FrmGrafik2 : Form
+    {
+        public FrmGrafik2()
+        {
+            InitializeComponent();
+        }
+        DbOtelEntities db = new DbOtelEntities();
+        private void FrmGrafik2_Load(object sender, EventArgs e)
+        {
+            var durumlar = db.OdaDurum();
+            foreach (var x in durumlar)
+            {
+                chartControl1.Series["Odalar"].Points.AddPoint(x.DurumAd, double.Parse(x.Adet.ToString()));
+            }
+        }
+    }
+}
